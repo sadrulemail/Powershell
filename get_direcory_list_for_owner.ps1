298f7094346926a0407f79ff12a6cb48912f5a3b
Sadrul <58506546+sadrulemail@users.noreply.github.com>
2023-02-28 09:39:01 +0600

    Add files via upload

---
+++ get_direcory_list_for_owner.ps1
@@ -0,0 +1,11 @@
+﻿
+$RootPath = '\\phl-svm-cifs-01.WCGCLINICAL.COM\phl_sql_non_prod_backup\'
+ 
+#gets a list of folders under the $startDirectory directory
+$directoryItems = Get-ChildItem $RootPath 
+#| Where-Object {$_.PSIsContainer -eq $true} | Sort-Object
+#$directoryItems
+ 
+ $List=$directoryItems | Select name, CreationTime, @{N="Owner";E={ (Get-Acl $_.FullName).Owner }}|Where-Object {$_.Owner -eq "WCGCLINICAL\salom-a"}
+ $List
+ #$FilterData=$List|Where-Object {$_.Owner -eq "WCGCLINICAL\salom-a"}
